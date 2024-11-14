e51a84a069f9283caa0c21ee96b6a9f4cf8c2b45
moranguinho282 <141155762+moranguinho282@users.noreply.github.com>
2024-11-14 07:56:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'para que serve o comando print em python?',
+    'O comamdo print exibe uma mensagem ou o valor de uma variavel na tela .'
+)
+
+criaCartao(
+    'GProgramação',
+    'O que é uma variável?',
+    'Uma variável é um espaço na memória do computador'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma Função',
+    'Uma função é um bloco codigo que executa alguma tarefa'
+)
+
+criaCartao(
+    'Programação',
+    'O que faz o comando if?',
+    'O comando if é usado para fazer uma verificação e executar código apenas se uma condição for verdadeira .'
+)
